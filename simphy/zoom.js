9f6ead1e45a656a9672ad09266453d5eb931ae66
anujpandey0212 <MS20BTECH11003@iith.ac.in>
2021-11-25 21:41:15 +0530

    ok

--- simphy/zoom.js
+++ simphy/zoom.js
@@ -1,45 +1,84 @@
         var variable_max=800;
         var variable_min=0;
+        var variable_max_x=800;
+        var variable_min_x=0
+        var variable_max_y=800;
         document.onkeydown=fun;
+        // var zoom_in=document.getElementById('zoom-in');
+        // zoom_in.onmousedown=zoom_in_func;
+        // function zoom_in_func(ev){
+        //     variable_min=variable_min-10;
+        //     variable_max=variable_max-10;
+        //     Render.lookAt(render, {
+        //         min: { x: variable_min, y: variable_min },
+        //         max: { x: variable_max, y: variable_max }
+        //     });
+        // }
+        // var zoom_out=document.getElementById('zoom-out');
+        // zoom_out.onmousedown=zoom_out_func;
+        // function zoom_out_func(ev){
+        //     variable_min=variable_min+10;
+        //     variable_max=variable_max+10;
+        //     Render.lookAt(render, {
+        //         min: { x: variable_min, y: variable_min },
+        //         max: { x: variable_max_x, y: variable_max }
+        //     });
+        // }
+        var left=document.getElementById('left');
+        left.onmousedown=left_func;
+        function left_func(ev){
+            variable_max_x=variable_max_x+10;
+            Render.lookAt(render, {
+                min: { x: variable_min, y: variable_min },
+                max: { x: variable_max_x, y: variable_max}
+            });
+        }
+        var right=document.getElementById('right');
+        right.onmousedown=right_func;
+        function right_func(ev){
+            variable_max_x=variable_max_x-10;
+            Render.lookAt(render, {
+                min: { x: variable_min, y: variable_min },
+                max: { x: variable_max_x, y: variable_max}
+            });
+        }
         var zoom_in=document.getElementById('zoom-in');
         zoom_in.onmousedown=zoom_in_func;
         function zoom_in_func(ev){
-            variable_min=variable_min-10;
-            variable_max=variable_max-10;
+            variable_max_y=variable_max_y-10;
             Render.lookAt(render, {
                 min: { x: variable_min, y: variable_min },
-                max: { x: variable_max, y: variable_max }
+                max: { x: variable_max_x, y: variable_max_y}
             });
         }
         var zoom_out=document.getElementById('zoom-out');
         zoom_out.onmousedown=zoom_out_func;
         function zoom_out_func(ev){
-            variable_min=variable_min+10;
-            variable_max=variable_max+10;
+            variable_max_y=variable_max_y+10;
             Render.lookAt(render, {
                 min: { x: variable_min, y: variable_min },
-                max: { x: variable_max, y: variable_max }
+                max: { x: variable_max_x, y: variable_max_y}
             });
         }
         function fun(ev){
             if(ev.code=="ArrowRight"){
-                variable_max=variable_max+10;
-                variable_min=variable_min+10;
-            }
-            if(ev.code=="ArrowUp"){
-                variable_min=variable_min+10;
-            }
+                variable_max_x=variable_max_x-10;
+                // variable_min=variable_min+10;
+             }
             if(ev.code=="ArrowLeft"){
-                variable_max=variable_max-10;
-                variable_min=variable_min-10;
+                variable_max_x=variable_max_x+10;
             }
-            if(ev.code=="ArrowDown"){
-                variable_min=variable_min-10;
-            }
+            // if(ev.code=="ArrowLeft"){
+            //     variable_max=variable_max-10;
+            //     variable_min=variable_min-10;
+            // }
+            // if(ev.code=="ArrowDown"){
+            //     variable_min=variable_min-10;
+            // }
 
             Render.lookAt(render, {
                 min: { x: variable_min, y: variable_min },
-                max: { x: variable_max, y: variable_max }
+                max: { x: variable_max, y: variable_max_x}
             });
         }
         Render.lookAt(render, {
